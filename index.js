c457bafcdb66d1801fad3c73ae7c1758faa46905
Hasan Ismail Abdulmalik <hasanqqsp@gmail.com>
2024-11-16 08:12:12 +0700

    fix : video status

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http,{
     cors: {
         origin: "*",
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST"],
     }
 });
 const { configDotenv } = require('dotenv');
