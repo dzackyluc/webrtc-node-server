127c30b5bd74f8226bea96ae192e95bdcc13ac7f
Hasan Ismail Abdulmalik <hasanqqsp@gmail.com>
2024-11-26 09:29:37 +0700

    using env for db_name

--- service.js
+++ service.js
@@ -7,15 +7,19 @@ class MongoDBService {
                 version: ServerApiVersion.v1,
                 strict: true,
                 deprecationErrors: true,
-            }
+            },
         });
     }
 
     async connect() {
         try {
             await this.client.connect();
-            await this.client.db("temanternak").command({ ping: 1 });
-            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+            await this.client
+                .db(process.env.DATABASE_NAME ?? "temanternak-prod")
+                .command({ ping: 1 });
+            console.log(
+                "Pinged your deployment. You successfully connected to MongoDB!"
+            );
         } catch (error) {
             console.error("Failed to connect to MongoDB", error);
         }
@@ -29,18 +33,19 @@ class MongoDBService {
         }
     }
 
-
-
     async updateConsultation(consultationId, userId, state, time) {
         try {
-            const result = await this.client.db("temanternak").collection("consultations").updateOne(
-                { _id: ObjectId.createFromHexString(consultationId) },
-                {
-                    $push: {
-                        call_logs: {userId, state, time }
+            const result = await this.client
+                .db(process.env.DATABASE_NAME ?? "temanternak-prod")
+                .collection("consultations")
+                .updateOne(
+                    { _id: ObjectId.createFromHexString(consultationId) },
+                    {
+                        $push: {
+                            call_logs: { userId, state, time },
+                        },
                     }
-                }
-            );
+                );
             return result;
         } catch (error) {
             console.error("Failed to update consultation", error);
